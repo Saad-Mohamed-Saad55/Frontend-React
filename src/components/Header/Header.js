ae94df4870b59d7230e33697475bcc382fca22c8
Saad-Mohamed-Saad55 <saad.mohamed55.is@gmail.com>
2024-08-17 14:26:12 +0300

    new

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import Link from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Header.css';
 import logo from './images/logo.jpg';
 import HeaderItems from "../HeaderItems/HeaderItems";
